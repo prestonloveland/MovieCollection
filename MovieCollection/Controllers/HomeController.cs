2731d65934bc371812cb060c1b2eba210441c621
Preston Loveland <62196792+prestonloveland@users.noreply.github.com>
2021-02-01 11:55:09 -0700

    Add project files.

---
+++ MovieCollection/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Action for Index Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //for Get to Movie Entry
+        [HttpGet]
+        public IActionResult MovieEntry()
+        {
+            return View();
+        }
+        //for Post to Movie Entry
+        [HttpPost]
+        public IActionResult MovieEntry(MovieEntry movie)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(movie);
+                return View("MovieList", TempStorage.Movies);
+            }
+            return View();
+        }
+        //Action For Movie List
+        public IActionResult MovieList()
+        {
+            return View(TempStorage.Movies);
+        }
+        //Action For poscasts page
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieCollection/Models/MovieEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Models
+{
+    //class to record information on a movie entry
+    public class MovieEntry
+    {
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public string Edited { get; set; }
+
+        public string Lent_To { get; set; }
+
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
